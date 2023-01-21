ce32d980146b7df42b5148c146d7c1b207da8eac
Alexlikx <104937223+Alexlikx@users.noreply.github.com>
2023-01-21 15:58:35 +0100

    Added patment form

--- src/pages/deposit.jsx
+++ src/pages/deposit.jsx
@@ -9,6 +9,7 @@ const Deposit = () => {
   const router = useRouter();
 
   const [User, setUser] = useState({});
+  const [amount, setAmount] = useState();
 
   const { status, data: session } = useSession({
     required: true,
@@ -36,7 +37,12 @@ const Deposit = () => {
       };
       GetBalance();
     }
-  }, [session, status]);
+    if (User.currency === "EUR" || User.currency === "USD") {
+      setAmount(10);
+    } else {
+      setAmount(400);
+    }
+  }, [session, status, User.currency]);
 
   function CloseMenu() {
     const elem = document.getElementById("menu__toggle");
@@ -53,43 +59,65 @@ const Deposit = () => {
     <>
       <Header />
       <div className="wrapper-content" onClick={() => CloseMenu()}>
-        <GooglePayButton
-          environment="TEST"
-          paymentRequest={{
-            apiVersion: 2,
-            apiVersionMinor: 0,
-            allowedPaymentMethods: [
-              {
-                type: "CARD",
-                parameters: {
-                  allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-                  allowedCardNetworks: ["MASTERCARD", "VISA"],
-                },
-                tokenizationSpecification: {
-                  type: "PAYMENT_GATEWAY",
+        <div className={styles.deposit__form}>
+          <h1 className={styles.deposit__title}>Пополнить счёт</h1>
+          <label htmlFor="deposit" className={styles.deposit__label}>
+            Введите сумму пополнения *
+          </label>
+          <input
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder={`${
+              User.currency === "EUR" || User.currency === "USD"
+                ? "10-800"
+                : "400-10000"
+            } ${User.currency}`}
+            className={styles.deposit__input}
+            id="deposit"
+            required
+          />
+          <GooglePayButton
+            environment="TEST"
+            paymentRequest={{
+              apiVersion: 2,
+              apiVersionMinor: 0,
+              allowedPaymentMethods: [
+                {
+                  type: "CARD",
                   parameters: {
-                    gateway: "example",
-                    gatewayMerchantId: "exampleGatewayMerchantId",
+                    allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                    allowedCardNetworks: ["MASTERCARD", "VISA"],
+                  },
+                  tokenizationSpecification: {
+                    type: "PAYMENT_GATEWAY",
+                    parameters: {
+                      gateway: "example",
+                      gatewayMerchantId: "exampleGatewayMerchantId",
+                    },
                   },
                 },
+              ],
+              merchantInfo: {
+                merchantId: "12345678901234567890",
+                merchantName: "Golden Slots",
               },
-            ],
-            merchantInfo: {
-              merchantId: "12345678901234567890",
-              merchantName: "Demo Merchant",
-            },
-            transactionInfo: {
-              totalPriceStatus: "FINAL",
-              totalPriceLabel: "Total",
-              totalPrice: "100.00",
-              currencyCode: "USD",
-              countryCode: "US",
-            },
-          }}
-          onLoadPaymentData={(paymentRequest) => {
-            console.log("load payment data", paymentRequest);
-          }}
-        />
+              transactionInfo: {
+                totalPriceStatus: "ESTIMATED",
+                totalPriceLabel: "Total",
+                totalPrice: "100",
+                currencyCode: "UAH",
+                countryCode: "UA",
+              },
+            }}
+            onLoadPaymentData={(paymentRequest) => {
+              router.push(
+                `/success?amount=${amount}&currency=${User.currency}`
+              );
+            }}
+            style={{ margin: "0 auto" }}
+            buttonType="short"
+          />
+        </div>
       </div>
     </>
   );
---
+++ src/pages/success/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Header from "../../components/Header";
+import SuccessIcon from "../../../public/success.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const Amount = () => {
+  const params = useRouter();
+  console.log(params.query);
+
+  function CloseMenu() {
+    const elem = document.getElementById("menu__toggle");
+    if (elem) {
+      elem.checked = false;
+    }
+    const elem2 = document.getElementById("menu__toggle_profile");
+    if (elem2) {
+      elem2.checked = false;
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <div
+        onClick={() => CloseMenu()}
+        style={{
+          width: "100vw",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#0e4609",
+          textAlign: "center",
+        }}
+      >
+        <Image
+          src={SuccessIcon}
+          alt="Успешное пополнение"
+          width={80}
+          height={80}
+          style={{ marginBottom: "20px" }}
+        />
+        <h1 style={{ marginBottom: "10px" }}>Успешное пополнение</h1>
+        <h2>
+          Вы пополнили свой счёт на {params.query.amount}{" "}
+          {params.query.currency}
+        </h2>
+        <Link
+          href="/"
+          style={{
+            marginTop: "15px",
+            color: "#fff",
+            fontSize: "20px",
+            textDecoration: "underline",
+          }}
+        >
+          На главную
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Amount;
